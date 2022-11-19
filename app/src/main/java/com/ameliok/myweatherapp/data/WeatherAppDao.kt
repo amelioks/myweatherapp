54b934d94fdce5593a7e2a52bb1c3e4d1c0c4310
Amalia Oktaviana <amalia.okv01@gmail.com>
2022-11-19 19:10:21 +0100

    adding WeatherEntity.kt

--- app/src/main/java/com/ameliok/myweatherapp/data/WeatherAppDao.kt
+++ app/src/main/java/com/ameliok/myweatherapp/data/WeatherAppDao.kt
@@ -1,14 +1,16 @@
 package com.ameliok.myweatherapp.data
 
 import androidx.lifecycle.LiveData
+import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.ameliok.myweatherapp.api.model.WeatherForecast
 import com.ameliok.myweatherapp.database.DatabaseConstants
 
+@Dao
 interface WeatherAppDao {
-    @Query("SELECT * FROM ${DatabaseConstants.TABLE_NAME} ORDER by closeApproachDate")
+    @Query("SELECT * FROM ${DatabaseConstants.TABLE_NAME} ORDER by dt")
     fun getWeatherForecastData(): LiveData<List<WeatherForecast>>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
---
+++ app/src/main/java/com/ameliok/myweatherapp/database/WeatherEntity.kt
@@ -0,0 +1,21 @@
+package com.ameliok.myweatherapp.database
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.ameliok.myweatherapp.api.model.City
+import com.ameliok.myweatherapp.api.model.Wind
+
+@Entity(tableName = DatabaseConstants.TABLE_NAME)
+data class WeatherEntity(
+    @PrimaryKey
+    val city: City,
+    val dt: Long,
+    val temp: Double,
+    val feelsLike: Double,
+    val tempMax: Double,
+    val tempMin: Double,
+    val wind: Wind,
+    val humidity: Int,
+    val pressure: Int,
+    val description: String
+)
