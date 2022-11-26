1dffc4ffbd981a84409a734d474b997d8721faed
Amalia Oktaviana <amalia.okv01@gmail.com>
2022-11-26 01:38:12 +0100

    adding motion layout

--- app/src/main/java/com/ameliok/myweatherapp/screen/detail/DetailFragment.kt
+++ app/src/main/java/com/ameliok/myweatherapp/screen/detail/DetailFragment.kt
@@ -1,10 +1,13 @@
 package com.ameliok.myweatherapp.screen.detail
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.constraintlayout.motion.widget.MotionLayout
 import androidx.fragment.app.Fragment
+import com.ameliok.myweatherapp.MainActivity
 import com.ameliok.myweatherapp.databinding.FragmentWeatherDetailBinding
 import com.ameliok.myweatherapp.utils.setWeatherIconUrl
 import com.ameliok.myweatherapp.utils.toDegree
@@ -23,6 +26,23 @@ class DetailFragment : Fragment() {
         binding.temperatureMax.text = weatherDetail.main.tempMax.toDegree()
         binding.temperatureMin.text = weatherDetail.main.tempMin.toDegree()
         binding.humidityLevel.text = weatherDetail.main.humidity.toString()
+
+        val motionLayout = binding.motionLayout
+        motionLayout.addTransitionListener(object : MotionLayout.TransitionListener {
+            override fun onTransitionStarted(p0: MotionLayout?, p1: Int, p2: Int) {
+            }
+
+            override fun onTransitionChange(p0: MotionLayout?, p1: Int, p2: Int, p3: Float) {
+            }
+
+            override fun onTransitionCompleted(p0: MotionLayout?, p1: Int) {
+                startActivity(Intent(requireActivity(), MainActivity::class.java))
+            }
+
+            override fun onTransitionTrigger(p0: MotionLayout?, p1: Int, p2: Boolean, p3: Float) {
+            }
+
+        })
 
         return binding.root
     }
